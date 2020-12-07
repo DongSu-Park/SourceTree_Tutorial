8cb08750c836ee508a1a86c5b558cd4a17273883
Park_Dong_Su <floreerin@gmail.com>
2020-12-07 16:04:04 +0900

    충돌 해결

--- app/src/main/java/com/example/sourcetree_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/sourcetree_tutorial/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.sourcetree_tutorial
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_toast.setOnClickListener {
+            Toast.makeText(this, "테스트 메세지 입니다.", Toast.LENGTH_LONG).show()
+        }
     }
 }
