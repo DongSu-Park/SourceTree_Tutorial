d727bcb83c5208c7bd31bc83e2ed444f3f06a75b
Park_Dong_Su <floreerin@gmail.com>
2020-12-07 17:44:45 +0900

    Add Log

--- app/src/main/java/com/example/sourcetree_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/sourcetree_tutorial/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sourcetree_tutorial
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,5 +14,7 @@ class MainActivity : AppCompatActivity(){
         btn_toast.setOnClickListener {
             Toast.makeText(this, "변경된 메세지 입니다.", Toast.LENGTH_LONG).show()
         }
+        
+        Log.d("tag","테스트 메세지 입니다.")
     }
 }
